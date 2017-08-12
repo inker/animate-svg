ed4c373f2bd2c66146f8d691a6aae6a59f67269d
Anton <anton.veselyev@gmail.com>
2017-08-12 20:27:38 +0300

    v1.0.2

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,3 @@
 export declare type SVGGeometryElement = SVGPathElement | SVGLineElement;
-declare var _default: (path: SVGGeometryElement, speed: number, reverse?: boolean) => Promise<Event>;
+declare const _default: (path: SVGGeometryElement, speed: number, reverse?: boolean) => Promise<Event>;
 export default _default;
